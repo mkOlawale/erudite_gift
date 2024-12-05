80efcdf59cb8fb12ab6981c33950df2b168d3f26
mkOlawale <abeebolaking1@gmail.com>
2024-12-05 10:09:17 -0800

    some change

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -12,7 +12,7 @@
                   <span class="count bg-success"></span>
                 </div>
                 <div class="profile-name">
-                  <h5 class="mb-0 font-weight-normal">Habeeb OLA</h5>
+                  <h5 class="mb-0 font-weight-normal">Erudite</h5>
                   <span>Gold</span>
                 </div>
               </div>
